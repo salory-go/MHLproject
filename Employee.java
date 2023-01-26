66c50c0dd5bdbac03323cd2386b9679163abda9d
EnCoUnTeR <102663083+salory-go@users.noreply.github.com>
2023-01-26 20:35:14 +0800

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,63 @@
+package domain;
+
+/**
+ * 这是一个javabean 和employee表映射
+ */
+public class Employee {
+    private Integer id;//防止空指针异常
+    private String empId;
+    private String pwd;
+    private String name;
+    private String job;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public Employee(Integer id, String empId, String pwd, String name, String job) {
+        this.id = id;
+        this.empId = empId;
+        this.pwd = pwd;
+        this.name = name;
+        this.job = job;
+    }
+
+    public Employee() {
+    }
+}
---
+++ MultiTableBean.java
@@ -0,0 +1,125 @@
+package domain;
+
+import java.util.Date;
+
+public class MultiTableBean {
+    private Integer id;
+    private Integer menuid;
+    private Integer nums;
+    private String billId;
+    private Double money;
+    private Integer diningTableId;
+    private Date billDate;
+    private String state;
+    private String name;
+    private String price;
+
+    public MultiTableBean(Integer id, Integer menuid, Integer nums, String billId, Double money, Integer diningTableId, Date billDate, String state, String name, String price) {
+        this.id = id;
+        this.menuid = menuid;
+        this.nums = nums;
+        this.billId = billId;
+        this.money = money;
+        this.diningTableId = diningTableId;
+        this.billDate = billDate;
+        this.state = state;
+        this.name = name;
+        this.price = price;
+    }
+
+    public MultiTableBean() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return  id +
+                "\t\t" + menuid +
+                "\t\t\t" + nums +
+                "\t\t\t" + money +
+                "\t" + diningTableId +
+                "\t\t" + billDate +
+                "\t\t" + state +
+                "\t\t" + name +
+                "\t\t" + price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getMenuid() {
+        return menuid;
+    }
+
+    public void setMenuid(Integer menuid) {
+        this.menuid = menuid;
+    }
+
+    public Integer getNums() {
+        return nums;
+    }
+
+    public void setNums(Integer nums) {
+        this.nums = nums;
+    }
+
+    public String getBillId() {
+        return billId;
+    }
+
+    public void setBillId(String billId) {
+        this.billId = billId;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    public Integer getDiningTableId() {
+        return diningTableId;
+    }
+
+    public void setDiningTableId(Integer diningTableId) {
+        this.diningTableId = diningTableId;
+    }
+
+    public Date getBillDate() {
+        return billDate;
+    }
+
+    public void setBillDate(Date billDate) {
+        this.billDate = billDate;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
